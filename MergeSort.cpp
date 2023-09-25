aabac12fef09dd1d7b1a6f30d258329f981978c9
Nand Kishore Singh <nand.kishore0109@gmail.com>
2023-09-25 13:35:20 +0530

    Modifiy

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,62 +1,63 @@
 #include<iostream>
 using namespace std;
-void merge(int arr[],int s,int mid,int e)
+void merge(int arr[],int& s,int& mid,int& e)
 {
     int l1=mid-s+1;
     int l2=e-mid;
     //creating two arrays 
     int *subarray1=new int[l1];
     int *subarray2=new int[l2];
-    int *finalarray=new int[l1+l2];
     //copying contents of arr into two subarrays
-    int i,j;
-    for(i=0;i<l1;i++)
-        subarray1[i]=arr[i];
-    for(j=0;j<l2;j++,i++)
-        subarray2[j]=arr[i];
-    i=s;
+    int j;
+    for(j=0;j<l1;j++)
+        subarray1[j]=arr[s+j];//s+j
+    for(j=0;j<l2;j++)
+        subarray2[j]=arr[mid+1+j];//mid+1+j
+    j=s;
     int sa1=0;
     int sa2=0;
     while(sa1<l1 && sa2<l2)
     {
         if(subarray1[sa1]<subarray2[sa2])
         {
-            finalarray[i]=subarray1[sa1];
+            arr[j]=subarray1[sa1];
             sa1++;
-            i++;
+            j++;
         }
         else if(subarray2[sa2]<subarray1[sa1])
         {
-            finalarray[i]=subarray2[sa2];
+            arr[j]=subarray2[sa2];
             sa2++;
-            i++;
+            j++;
         }
         else if(subarray1[sa1]==subarray2[sa2])
         {
-            finalarray[i]=subarray1[sa1];
-            finalarray[i+1]=subarray2[sa1];
-            i=i+2;
+            arr[j]=subarray1[sa1];
+            arr[j+1]=subarray2[sa2];
+            j=j+2;
             sa1++;
             sa2++;
         }
     }
     while(sa1<l1)
     {
-        finalarray[i]=subarray1[sa1];
+        arr[j]=subarray1[sa1];
         sa1++;
-        i++;
+        j++;
     }
     while(sa2<l2)
     {
-        finalarray[i]=subarray2[sa2];
-        i++;
+        arr[j]=subarray2[sa2];
+        j++;
         sa2++;
     }
+    delete []subarray1;
+    delete []subarray2;
     //copying back to arr original array
-    for(i=s;i<e;i++)
-    {
-        arr[i]=finalarray[i];
-    }
+    // for(i=s;i<e;i++)
+    // {
+    //     arr[i]=arr[i];
+    // }
 }
 void mergeSort(int arr[],int s,int e)
 {
@@ -69,7 +70,7 @@ void mergeSort(int arr[],int s,int e)
 }
 int main()
 {
-    int n;
+    int n=0;
     cout<<"Enter array length"<<endl;
     cin>>n;
     int *arr=new int[n];
